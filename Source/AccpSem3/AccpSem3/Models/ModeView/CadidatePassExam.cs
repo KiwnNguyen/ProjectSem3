9f9b2db1921c6084e090e23c0b74dd3450be1f97
KiwnNguyen <128109332+KiwnNguyen@users.noreply.github.com>
2024-06-07 21:10:03 +0700

    tạo lịch phỏng vấn cho cadidate

---
+++ Source/AccpSem3/AccpSem3/Models/ModeView/CadidatePassExam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccpSem3.Models.ModeView
+{
+    public class CadidatePassExam
+    {
+        public int id { get; set; }
+        public Nullable<int> id_member { get; set; }
+        public string fullname { get; set; }
+        public string email { get; set; }
+        public Nullable<int> id_vacancy { get; set; }
+        public string title_vacancy { get; set; }
+        public string concern_person { get; set; }
+        public Nullable<int> status { get; set; }
+        public Nullable<int> score { get; set; }
+        public string answer_of_cadidate { get; set; }
+        public Nullable<System.DateTime> created_at { get; set; }
+        public Nullable<System.DateTime> updated_at { get; set; }
+        public string submit_cadidate_cadidate { get; set; }
+        public Nullable<System.DateTime> expire_date { get; set; }
+    }
+}
